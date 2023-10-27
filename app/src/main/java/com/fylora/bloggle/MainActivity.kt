237584cb06c9c35f6c6cc53a1590716f84e966d6
fylora <fylora189@gmail.com>
2023-10-27 20:22:00 +0300

    Initial commit

---
+++ app/src/main/java/com/fylora/bloggle/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.fylora.bloggle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fylora.bloggle.ui.theme.BloggleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BloggleTheme {
+
+            }
+        }
+    }
+}
